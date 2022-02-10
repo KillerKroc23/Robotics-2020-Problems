0e426592da59c1acc80db1c0e14fa70d0c49e422
Christopher Ramroop <78105128+KillerKroc23@users.noreply.github.com>
2022-02-10 12:19:46 -0500

    Add files via upload

---
+++ Robotics 2020 Problems/No More Shouting.java
@@ -0,0 +1,12 @@
+
+public class StringConverter{
+    public static void Main(Sring[] args){
+        String changeCase = "text to change";
+        System.out.println( changeCase);
+
+        String result;
+        result = changeCase.toUpperCase();
+
+        System.out.println(result);
+    }
+}
